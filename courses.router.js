4ef6a1b952a6f8cd447726d4c498da1256f5594a
yashsati29 <yashsati29@gmail.com>
2025-01-06 18:46:15 +0530

    create coursesRouter for routing course related routes

---
+++ courses.router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const coursesRouter = express.Router();
+
+coursesRouter.get("/view", (req, res) => {});
+
+coursesRouter.post("/purchase", (req, res) => {});
+
+module.exports = coursesRouter;
--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 const userRouter = require("./user.router");
 app.use("/user", userRouter);
 
-app.get("/courses/view", (req, res) => {});
-app.post("/courses/purchase", (req, res) => {});
+const coursesRouter = require("./courses.router");
+app.use("/courses", coursesRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
