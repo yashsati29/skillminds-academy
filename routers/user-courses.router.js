ec533171c4043a2878116d4bddff4398c10be245
yashsati29 <yashsati29@gmail.com>
2025-01-07 15:35:46 +0530

    feat: implement course viewing with JWT authentication and error handling

--- routers/user-courses.router.js
+++ routers/user-courses.router.js
@@ -1,13 +1,37 @@
 const { Router } = require("express");
+const jwt = require("jsonwebtoken");
+
+const { JWT_TOKEN_SECRET } = require("../config");
+const CoursesModel = require("../models/courses.model");
+
 const router = Router();
 
-router.get("/view", (req, res) => {});
-router.post("/create", (req, res) => {});
-router.put("/update", (req, res) => {});
-router.delete("/delete", (req, res) => {});
+router.get("/view", (req, res) => {
+  const token = req.headers.token;
+  const findCourses = (query) =>
+    CoursesModel.find(query)
+      .then((courses) =>
+        res.status(200).json({ courses, message: "Fetched all courses!" })
+      )
+      .catch(() =>
+        res.status(500).json({
+          message: "Error while fetching the courses, please try again!",
+        })
+      );
+
+  if (!token) findCourses();
+  else {
+    const { userId: creatorId } = jwt.verify(token, JWT_TOKEN_SECRET);
+    findCourses({ creatorId });
+  }
+});
+
+router.get("/purchased", (req, res) => {});
 
 router.post("/purchase", (req, res) => {});
 
-router.get("/purchased", (req, res) => {});
+router.put("/update", (req, res) => {});
+
+router.delete("/delete", (req, res) => {});
 
 module.exports = router;
