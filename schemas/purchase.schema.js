f35317e4405def92d39435e8868113caa4163254
yashsati29 <yashsati29@gmail.com>
2025-01-07 18:27:18 +0530

    feat: enforce unique constraints on creatorId in course schema and userId/courseId in purchase schema

--- schemas/purchase.schema.js
+++ schemas/purchase.schema.js
@@ -2,8 +2,8 @@ const { Schema } = require("mongoose");
 const { ObjectId } = Schema;
 
 const PurchaseSchema = new Schema({
-  userId: { type: ObjectId, ref: "users", required: true },
-  courseId: { type: ObjectId, ref: "courses", required: true },
+  userId: { type: ObjectId, ref: "users", unique: true, required: true },
+  courseId: { type: ObjectId, ref: "courses", unique: true, required: true },
 });
 
 module.exports = PurchaseSchema;
